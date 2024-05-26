3dfec0dc60e9891ecb53b01aa3d4d824426710c6
WillianChavez <williamchavezamaya@gmail.com>
2024-05-25 19:57:39 -0600

    first commit

---
+++ app/src/main/java/com/example/proyecto_innovacion_gt03_03_tema11/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.proyecto_innovacion_gt03_03_tema11.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
