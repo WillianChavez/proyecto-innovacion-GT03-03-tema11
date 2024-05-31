3ac1b3a1efd2d98a515eb37fad48bb9474f90958
Willian Alexander Chávez Servellón <williamchavezamaya@gmail.com>
2024-05-31 11:37:19 -0600

    Agregando mapa de google maps

--- app/src/main/java/com/example/proyecto_innovacion_gt03_03_tema11/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_innovacion_gt03_03_tema11/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.proyecto_innovacion_gt03_03_tema11
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.Menu
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -12,44 +13,29 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyecto_innovacion_gt03_03_tema11.databinding.ActivityMainBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.appBarMain.toolbar)
-
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null)
-                    .setAnchorView(R.id.fab).show()
-        }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    override fun onMapReady(googleMap: GoogleMap) {
+        val elsalvador = LatLng(13.70817959983269, -89.22310808675819)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(elsalvador)
+                .title("Marker")
+        )
     }
 }
--- app/src/main/java/com/example/proyecto_innovacion_gt03_03_tema11/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/proyecto_innovacion_gt03_03_tema11/ui/home/HomeFragment.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.proyecto_innovacion_gt03_03_tema11.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.proyecto_innovacion_gt03_03_tema11.R
+
+import com.example.proyecto_innovacion_gt03_03_tema11.databinding.FragmentHomeBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentHomeBinding? = null
+
+    private lateinit var mMap: GoogleMap
+
+    private val homeViewModel : HomeViewModel by viewModels()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,17 +34,27 @@ class HomeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val root: View = inflater.inflate(R.layout.fragment_home,container,false)
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.id_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val salvador = LatLng(.703093009418746, -89.21658495448729)
+
+        mMap = googleMap
+        mMap.addMarker(
+            MarkerOptions().position(salvador)
+                .title("Marker")
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(salvador))
+
     }
 
     override fun onDestroyView() {
